99cc20aefb84e884206f0e4e5bda3e2df3b30688
moms2700 <85704249+moms2700@users.noreply.github.com>
2023-10-16 00:53:56 +0200

    Update Web3context.tsx

--- frontend/src/app/components/Web3context.tsx
+++ frontend/src/app/components/Web3context.tsx
@@ -1,34 +1,41 @@
 import { createContext, useContext, useState, ReactNode } from "react";
+import { TezosToolkit } from "@taquito/taquito";
 import { TempleWallet } from '@temple-wallet/dapp';
 
+const rpcURL = "https://ghostnet.ecadinfra.com/";
+type WalletProviderProps = {
+  children: ReactNode;
+};
 type WalletContextType = {
   wallet: string | null;
   handleConnectWallet: () => void;
   handleDisconnectWallet: () => void;
+  executeContractFunction: (functionName: string, args: any[]) => Promise<void>;
 };
 
 const WalletContext = createContext<WalletContextType | null>(null);
 
-type WalletProviderProps = {
-  children: ReactNode;
-};
-
 export function WalletProvider({ children }: WalletProviderProps) {
   const [wallet, setWallet] = useState<string | null>(null);
 
+  const tezos = new TezosToolkit(rpcURL);
+
   async function handleConnectWallet() {
     if (await TempleWallet.isAvailable()) {
-      const mywallet = new TempleWallet('MyAwesomeDapp');
-      try {
-        await mywallet.connect('ghostnet');
-        const pkh = await mywallet.getPKH();
-        setWallet(pkh);
-      } catch (err) {
-        console.error(err);
-      }
+      const templeWallet = new TempleWallet('MyAwesomeDapp');
+      await templeWallet.connect('ghostnet');
+      const pkh = await templeWallet.getPKH();
+      setWallet(pkh);
+      tezos.setWalletProvider(templeWallet);  // Setting Temple as the wallet provider for Taquito
     } else {
       console.error("Temple Wallet not available");
     }
+  }
+
+  async function executeContractFunction(functionName: string, args: any[]) {
+    const contract = await tezos.wallet.at("KT1TM1HZQDtLiPuYhbWYmH68ojeawr3VQw6h");
+    const operation = await contract.methods[functionName](...args).send();
+    await operation.confirmation();
   }
 
   function handleDisconnectWallet() {
@@ -36,7 +43,7 @@ export function WalletProvider({ children }: WalletProviderProps) {
   }
 
   return (
-    <WalletContext.Provider value={{ wallet, handleConnectWallet, handleDisconnectWallet }}>
+    <WalletContext.Provider value={{ wallet, handleConnectWallet, handleDisconnectWallet, executeContractFunction }}>
       {children}
     </WalletContext.Provider>
   );
